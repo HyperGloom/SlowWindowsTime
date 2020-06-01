158fef78275a2f96de0e76a8ba15b5d75788cd04
Olzhas <34390275+HyperGloom@users.noreply.github.com>
2020-06-01 14:38:20 +0600

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strCmdText;
+            Console.WriteLine("Set hour");
+            int hour = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Set minute");
+            int minute = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Set second");
+            int second = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("how much slow down time?(if you write 2, it will 2 times slower");
+            int slowValue = Int32.Parse(Console.ReadLine())*2;
+            int Timedelay = 500;
+
+            for (int i = 0; ;i++)
+            {
+                
+                for(int j=0;j<slowValue;j++)
+                {
+                    Thread.Sleep(Timedelay);
+                    strCmdText = "/C time " + hour.ToString() + ":" + minute.ToString() + ":" + second.ToString() + ",0";
+                    System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+                }
+                second++;
+                if (second == 60)
+                {
+                    second = 0;
+                    minute++;
+                }
+                if (minute == 60)
+                {
+                    minute = 0;
+                    hour++;
+                }
+            }
+            
+        }
+    }
+}
